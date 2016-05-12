cc32c72a904f96c042dc5bc74e674726856bf01f
Matt Travi <programmer@travi.org>
2016-05-11 23:28:18 -0500

    added index component

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,6 +4,7 @@ import '../src/theme/theme.scss';
 
 function loadStories() {
     require('../stories/primary-nav');
+    require('../stories/index');
     require('../stories/resource-list');
     require('../stories/resource');
     require('../stories/errors');
