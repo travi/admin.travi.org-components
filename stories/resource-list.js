04264c51e441de38e5cc685f930be8b673d41e37
Matt Travi <programmer@travi.org>
2016-06-22 00:23:50 -0500

    added the storybook info addon and enabled for the resource-list component to close #31

--- stories/resource-list.js
+++ stories/resource-list.js
@@ -5,17 +5,33 @@ import createResourceList from '../src/resources/list/maybe-list';
 const ResourceList = createResourceList(React);
 
 storiesOf('Resource List', module)
-    .add('empty list', () => <ResourceList resourceType="foo" resources={[]}/>)
-    .add('rides list', () => <ResourceList resourceType="rides" resources={[
-        { id: 1, displayName: 'corvette', links: [] },
-        { id: 2, displayName: 'truck', links: [] },
-        { id: 3, displayName: 'camaro', links: [] }
-    ]}/>)
-    .add('users list', () => <ResourceList resourceType="users" resources={[
-        {
-            id: 1,
-            displayName: 'Matt Travi',
-            thumbnail: {src: 'https://www.gravatar.com/avatar/f69785efc7d990da20f1ab49fc2a6648?size=32'},
-            links: []
-        }
-    ]}/>);
+    .addWithInfo(
+        'empty list',
+        'this is the empty state',
+        () => <ResourceList resourceType="foo" resources={[]}/>
+    )
+    .addWithInfo(
+        'rides list',
+        'list of rides',
+        () => (
+            <ResourceList resourceType="rides" resources={[
+                { id: 1, displayName: 'corvette', links: [] },
+                { id: 2, displayName: 'truck', links: [] },
+                { id: 3, displayName: 'camaro', links: [] }
+            ]}/>
+        )
+    )
+    .addWithInfo(
+        'users list',
+        'list of users',
+        () => (
+            <ResourceList resourceType="users" resources={[
+                {
+                    id: 1,
+                    displayName: 'Matt Travi',
+                    thumbnail: {src: 'https://www.gravatar.com/avatar/f69785efc7d990da20f1ab49fc2a6648?size=32'},
+                    links: []
+                }
+            ]}/>
+        )
+    );
