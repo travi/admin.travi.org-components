9d341d1c05cceaa2dacc6815dbe0f09e286ad842
Matt Travi <programmer@travi.org>
2016-08-02 15:40:12 -0400

    centered the loading indicator

--- src/atoms/loading-indicators/page.js
+++ src/atoms/loading-indicators/page.js
@@ -2,5 +2,5 @@ import React from 'react';
 import CircularProgress from 'material-ui/CircularProgress';
 
 export default function PageLoading() {
-    return <CircularProgress color={'#c80000'} />;
+    return <CircularProgress color={'#c80000'} style={{margin: '10px auto', display: 'block'}} />;
 }
