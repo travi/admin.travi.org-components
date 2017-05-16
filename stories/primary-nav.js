1cd7716108241c92d05dd0233672ce243cacf9b2
Matt Travi <programmer@travi.org>
2017-05-15 21:36:45 -0500

    refactor(lint): updated to align to newly enforced lint rule

--- stories/primary-nav.js
+++ stories/primary-nav.js
@@ -4,9 +4,11 @@ import PrimaryNav from '../src/theme/nav/primary-nav';
 
 storiesOf('Primary Nav', module)
   .add('empty nav', () => <PrimaryNav primaryNav={[]} />)
-  .add('populated nav', () => <PrimaryNav
-    primaryNav={[
-      {text: 'rides', path: '/foo'},
-      {text: 'users', path: '/bar'}
-    ]}
-  />);
+  .add('populated nav', () => (
+    <PrimaryNav
+      primaryNav={[
+        {text: 'rides', path: '/foo'},
+        {text: 'users', path: '/bar'}
+      ]}
+    />
+  ));
